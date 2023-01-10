e009b34dfc68f99a49d63ac60246a9847dff33b5
DeMoManique <114985910+DeMoManique@users.noreply.github.com>
2023-01-10 21:06:58 +0100

    Random n' Greedy works

---
+++ src/GreedyAI.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+public class GreedyAI extends Ai{
+
+    ArrayList<Integer> bestMoves;
+    GreedyAI(Controller controller, Model model) {
+        super(controller, model);
+        bestMoves = new ArrayList<Integer>();
+    }
+
+    
+    public void placePiece() {
+        bestMoves.clear();
+        int best = 0;
+        int move;
+        for(int i = 0; i < possibleMoves.size(); i++){
+            if (possibleMoves.get(i).toBeFlipped.size() > best){
+                best = possibleMoves.get(i).toBeFlipped.size();
+                bestMoves.clear();
+                bestMoves.add(i);
+            }
+            else if(possibleMoves.get(i).toBeFlipped.size() == best) bestMoves.add(i);
+        }
+        move = bestMoves.get((int) (Math.random() * bestMoves.size()));
+        model.tryMove(possibleMoves.get(move).getTile().getRow(),possibleMoves.get(move).getTile().getCol());
+    }
+    
+}
